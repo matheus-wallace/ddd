b2c0b71f6fb255bf940a4e210998f66496f3b91a
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2025-12-07 17:22:23 -0300

    feat: add delete answer user case

--- src/domain/forum/application/repository/answers-repository.ts
+++ src/domain/forum/application/repository/answers-repository.ts
@@ -1,5 +1,7 @@
 import { Answer } from '../../enterprise/entities/answer'
 
 export interface AnswersRepository {
+  findById(slug: string): Promise<Answer | null>
   create(answer: Answer): Promise<void>
+  delete(answer: Answer): Promise<void>
 }
