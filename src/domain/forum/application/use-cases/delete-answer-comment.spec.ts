3eba020bf623342ee5ad3004a955dddcff11abf7
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2025-12-15 22:16:22 -0300

    feat: add new error classes

--- src/domain/forum/application/use-cases/delete-answer-comment.spec.ts
+++ src/domain/forum/application/use-cases/delete-answer-comment.spec.ts
@@ -2,6 +2,7 @@ import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-an
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { DeleteAnswerCommentUseCase } from './delete-answer-comment'
 import { makeAnswerComment } from 'test/factories/make-answer-comment'
+import { NotAllowedError } from './errors/not-allowed-error'
 
 let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
 let sut: DeleteAnswerCommentUseCase
@@ -28,12 +29,12 @@ describe('Delete Answer Comment', () => {
     })
     await inMemoryAnswerCommentsRepository.create(answerComment)
 
-    await expect(() => {
-      return sut.execute({
-        answerCommentId: answerComment.id.toString(),
+    const result = await sut.execute({
+      answerCommentId: answerComment.id.toString(),
 
-        authorId: 'author-2',
-      })
-    }).rejects.toBeInstanceOf(Error)
+      authorId: 'author-2',
+    })
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(NotAllowedError)
   })
 })
