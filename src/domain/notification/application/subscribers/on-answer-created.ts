1c381afab7ca76836e25684deb1ce59b604c95de
= <=>
2026-03-07 22:12:53 -0300

    feat: new events when answe is created

---
+++ src/domain/notification/application/subscribers/on-answer-created.ts
@@ -0,0 +1,38 @@
+import { DomainEvents } from '@/core/events/domain-events'
+import { EventHandler } from '@/core/events/event-handler'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/question-repository'
+import { AnswerCreatedEvents } from '@/domain/forum/enterprise/events/answer-created-envents'
+
+import { SendNotificationUseCase } from '@/domain/notification/application/use-cases/send-notification'
+
+export class OnAnswerCreated implements EventHandler {
+  constructor(
+    private questionsRepository: QuestionsRepository,
+    private sendNotification: SendNotificationUseCase,
+  ) {
+    this.setupSubscriptions()
+  }
+
+  setupSubscriptions(): void {
+    DomainEvents.register(
+      this.sendNewAnswerNotification.bind(this),
+      AnswerCreatedEvents.name,
+    )
+  }
+
+  private async sendNewAnswerNotification({ answer }: AnswerCreatedEvents) {
+    const question = await this.questionsRepository.findById(
+      answer.questionId.toString(),
+    )
+
+    if (question) {
+      await this.sendNotification.execute({
+        recipientId: question.authorId.toString(),
+        title: `Nova resposta em "${question.title
+          .substring(0, 40)
+          .concat('...')}"`,
+        content: answer.excerpt,
+      })
+    }
+  }
+}
--- src/domain/notification/application/use-cases/send-notification.ts
+++ src/domain/notification/application/use-cases/send-notification.ts
@@ -1,15 +1,15 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Either, right } from '@/core/either'
 import { Notification } from '../../enterprise/entitites/notification'
-import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { NotificationsRepository } from '../repositories/notifications-repositories'
 
-interface SendNotificationUseCaseRequest {
+export interface SendNotificationUseCaseRequest {
   recipientId: string
   title: string
   content: string
 }
 
-type SendNotificationUseCaseResponse = Either<
+export type SendNotificationUseCaseResponse = Either<
   null,
   {
     notification: Notification
